87492c3816b97ea7db96314a63da9a8cbb364520
Dasha Day Hisoler <dhisoler@hawk.iit.edu>
2018-11-12 14:47:35 -0600

    Hide or show html elements based on JavaScript presence

--- pp-04/site.js
+++ pp-04/site.js
@@ -9,6 +9,12 @@
 
     var doorbell = document.querySelector('#doorbell');
     var bellAudio = new Audio('media/doorbell.mp3');
+    var player = document.querySelector('#nojs-doorbell')
+
+    // Show button if JavaScript is working
+    player.classList.replace("visible","not-visible");
+    doorbell.classList.replace("not-visible","visible");
+
     // Listen for click events on the doorbell
     doorbell.addEventListener('click', function() {
       bellAudio.play();
