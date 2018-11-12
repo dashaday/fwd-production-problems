b1a4fb95a0abd9fe7981aaa90a17e80c3d8ec0e2
Dasha Day Hisoler <dhisoler@hawk.iit.edu>
2018-11-12 12:41:41 -0600

    Improve JavaScript semantics

--- pp-04/site.js
+++ pp-04/site.js
@@ -1,4 +1,19 @@
-function ringDoorbell() {
-  var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
+(function(){
+  if (typeof(document.querySelector)==="undefined") {
+    return;
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Add a js class on the <html> element...
+    document.querySelector('html').className = 'js';
+
+    var doorbell = document.querySelector('#doorbell');
+    var bellAudio = new Audio('media/doorbell.mp3');
+    // Listen for click events on the doorbell
+    doorbell.addEventListener('click', function() {
+      bellAudio.play();
+    });
+
+  });
+
+})();
