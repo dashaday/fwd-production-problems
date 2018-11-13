00b9a6a6f7d32fc2ec6a5c2680a0b4766522b0a2
Dasha Day Hisoler <dhisoler@hawk.iit.edu>
2018-11-12 22:33:40 -0600

    Organize and add comments

--- pp-04/site.js
+++ pp-04/site.js
@@ -13,24 +13,24 @@
     var doorbell = document.querySelector('#doorbell');
     // Sound icon
     var icon = document.querySelector('#sound-icon');
-
     // Doorbell audio
     var bellAudio = new Audio('media/doorbell.mp3');
-    var audioDuration = bellAudio.duration;
 
     // Hide audio player
     player.classList.replace("visible","not-visible");
     // Show clickable button
     doorbell.classList.replace("not-visible","visible");
 
-    function hideIcon() {
-      icon.classList.replace("visible","not-visible");
-    }
-
+    // Plays audio, shows sound image
     function buttonClicked() {
       bellAudio.play();
       icon.classList.replace("not-visible","visible");
       setTimeout(hideIcon, 2000);
+    }
+
+    // Hides sound icon after 2 seconds (audio duration)
+    function hideIcon() {
+      icon.classList.replace("visible","not-visible");
     }
 
     // Listen for click events on the doorbell
@@ -38,18 +38,18 @@
       buttonClicked();
     });
 
+    // Play audio when button is selected through tab and 'Enter' is pressed
     doorbell.addEventListener('keydown', function(e) {
       if (e.key === 'Enter') {
         buttonClicked();
       }
     });
 
+    // Play audio when 'd' is pressed anywhere on the viewport
     document.addEventListener('keydown', function(e) {
       if (e.key === 'd') {
         buttonClicked();
       }
     });
-
   });
-
 })();
