31e0d04a1e14d89e7d1a0d1fd5c8e4483e3077e6
Dasha Day Hisoler <dhisoler@hawk.iit.edu>
2018-11-12 22:32:03 -0600

    Play sound when 'Enter' or 'd' is pressed

--- pp-04/site.js
+++ pp-04/site.js
@@ -11,29 +11,42 @@
     var player = document.querySelector('#nojs-doorbell')
     // Object that appears with JavaScript
     var doorbell = document.querySelector('#doorbell');
+    // Sound icon
+    var icon = document.querySelector('#sound-icon');
 
     // Doorbell audio
     var bellAudio = new Audio('media/doorbell.mp3');
+    var audioDuration = bellAudio.duration;
 
     // Hide audio player
     player.classList.replace("visible","not-visible");
     // Show clickable button
     doorbell.classList.replace("not-visible","visible");
 
+    function hideIcon() {
+      icon.classList.replace("visible","not-visible");
+    }
+
+    function buttonClicked() {
+      bellAudio.play();
+      icon.classList.replace("not-visible","visible");
+      setTimeout(hideIcon, 2000);
+    }
+
     // Listen for click events on the doorbell
     doorbell.addEventListener('click', function() {
-      bellAudio.play();
+      buttonClicked();
     });
 
     doorbell.addEventListener('keydown', function(e) {
       if (e.key === 'Enter') {
-        bellAudio.play();
+        buttonClicked();
       }
     });
 
     document.addEventListener('keydown', function(e) {
       if (e.key === 'd') {
-        bellAudio.play();
+        buttonClicked();
       }
     });
 
